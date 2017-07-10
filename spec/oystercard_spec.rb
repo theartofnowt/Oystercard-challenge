a9ba72adb1d819fe8e31d1919ffd0e508cc335d4
artemis <artemisxen@gmail.com>
2017-07-10 13:51:02 +0100

    adds balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard){ described_class.new }
 
-
-
-
-
-
-
-
-
-
+  it "tests that an initialized card has balance of 0" do
+    expect(oystercard.balance).to be 0
+  end
 
 end
