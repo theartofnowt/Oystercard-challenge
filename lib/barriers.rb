18e6d435321ea21e044386744f2226e2f04d78cf
artemis <artemisxen@gmail.com>
2017-07-10 16:31:19 +0100

    8 touch in and out

---
+++ lib/barriers.rb
@@ -0,0 +1,19 @@
+class Barriers
+
+  def initialize
+    @cards = []
+  end
+
+  def touch_in(card)
+    @cards << card
+  end
+
+  def touch_out(card)
+    @cards.delete(card)
+  end
+
+  def in_journey?(card)
+    true unless touch_out(card)
+  end
+
+end
---
+++ spec/barriers_spec.rb
@@ -0,0 +1,22 @@
+require 'barriers'
+require 'oystercard'
+
+describe "Barriers" do
+  subject(:barriers) { Barriers.new }
+  let(:card) {Oystercard.new}
+
+  it "test if the card is touched in?" do
+    expect(barriers.touch_in(card)).to include card
+  end
+
+  it "test if the card is touched out?" do
+    barriers.touch_in(card)
+    expect(barriers.touch_out(card)).to eq card
+  end
+
+  it "test if the card is still in journey" do
+    barriers.touch_in(card)
+    barriers.touch_out(card)
+    expect(barriers.in_journey?(card)).to be true
+  end
+end
